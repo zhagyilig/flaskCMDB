594299dc23ed1c292a9d09a8b7cb117a0d3bccbb
xtrdb.net <813415154@qq.com>
2017-12-30 14:21:31 +0800

    添加服务器添加代码,异步获取后端数据:mushroom:

--- idc_list.py
+++ idc_list.py
@@ -5,7 +5,6 @@ from flask import Flask, request, render_template, redirect, session
 import MySQLdb as mysql
 import json
 
-# 自定义模块
 
 conn= mysql.connect(    # 连接MySQL
         host='localhost',
@@ -17,7 +16,7 @@ conn= mysql.connect(    # 连接MySQL
         )
 
 cur = conn.cursor()
-# conn.autocommit(True)
+conn.autocommit(True)
 # cur.execute("select * from flask_user")   # 测试连接库
 # print(cur.fetchall())
 
@@ -27,19 +26,47 @@ app = Flask(__name__)
 def index():
     return render_template('idc_index.html')
 
+## app ==> idc
+# 显示idc地域分布
 @app.route('/idc_list')
 def idc_list():
     cur.execute('select * from idc_list')
     res = cur.fetchall()
     return json.dumps(res)
 
+# 添加idc page.
 @app.route('/add_idc',methods=['post'])
 def addidc():
     idc_name = request.form.get('name')
     sql = 'insert into idc_list(name) values("%s")' %(idc_name)
     print(sql)
     cur.execute(sql)
-    conn.commit()
+    # conn.commit()
+    return 'ok'
+
+
+## app ==> pc
+@app.route('/pc')
+def pc():
+    return render_template('/pc.html')
+
+@ app.route('/pclist')
+def pclist():
+    cur.execute( 'select * from pc')
+    res = cur.fetchall()
+    return json.dumps(res)
+
+# 添加服务器信息
+@app.route('/addpc',methods=['post'])
+def addpc():
+    ip = request.form.get('ip')
+    mem = request.form.get('mem')
+    idc_id = request.form.get('idc_id')
+    create_time = request.form.get('create_time')
+    sql = 'insert into pc(ip,mem,idc_id) values("%s",%s,%s)' %(ip,mem,idc_id)
+    print('*'*100)
+    print(sql)
+    cur.execute(sql)
     return 'ok'
 
 if __name__ == '__main__':
